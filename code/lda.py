002d0bca3a6857907e26e28fb52cd45cd7153f1c
QuanMa722 <663244693@qq.com>
2024-05-23 17:30:07 +0800

    update

--- code/lda.py
+++ code/lda.py
@@ -16,7 +16,7 @@ def get_topic():
         with open(file="../data/stopwords.txt", mode="r", encoding="utf-8") as stop_file:
             stopwords: list = stop_file.read().splitlines()
 
-        with open(file="../data/process4.txt", mode="r", encoding="utf-8") as f:
+        with open(file="../data/process12.txt", mode="r", encoding="utf-8") as f:
             comment_dict_list: list = f.readlines()
 
         comment_list = [eval(item)["评论内容"] for item in comment_dict_list]
@@ -75,10 +75,10 @@ def get_topic():
 def get_file(topic_words):
     try:
 
-        with open(file="../data/process4.txt", mode="r", encoding="utf-8") as f1:
+        with open(file="../data/process12.txt", mode="r", encoding="utf-8") as f1:
             comment_dict_list: list = f1.readlines()
 
-        with open(f"../data/lda4.txt", "a", encoding="utf-8") as f2:
+        with open(f"../data/lda12.txt", "a", encoding="utf-8") as f2:
             for comment in comment_dict_list:
                 corpus = jieba.lcut(eval(comment)["评论内容"])
                 for word in corpus:
@@ -96,4 +96,4 @@ def get_file(topic_words):
 if __name__ == '__main__':
     top_words_list = get_topic()
     # processing the original data after determining the list of deactivated words
-    # get_file(top_words_list)
+    get_file(top_words_list)
